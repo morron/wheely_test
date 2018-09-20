e73e601bf9b0720be0bba75e720603113c1db458
iamthemorron <iamthemorron@gmail.com>
2018-09-20 12:06:37 +0300

    min price api spec

--- api/spec/price_calculation_spec.rb
+++ api/spec/price_calculation_spec.rb
@@ -5,9 +5,14 @@ require_relative '../lib/price_calculation'
 RSpec.describe PriceCalculation do
   subject { described_class.new }
   let(:rate) { Rate.new(base: 250, perKm: 20, perMin: 20, min: 500) }
+  let(:rate_with_min_price) { Rate.new(base: 250, perKm: 20, perMin: 20, min: 50_000) }
   let(:direction_result) { ::Wheelysvc::DirectionResult.new(distance: 25178, duration: 1957) }
 
   it "#call" do
     expect(subject.(rate, direction_result)).to eql 1406
+  end
+
+  it "returns min price" do
+    expect(subject.(rate_with_min_price, direction_result)).to eql 50_000
   end
 end
